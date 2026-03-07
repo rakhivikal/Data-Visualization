244188f6cb42b42d7e8834a60454bc27463e76cd
Rakhi <rakhivikal008@gmail.com>
2026-03-07 10:15:08 +0530

    python program

---
+++ Class_Work/Program.py
@@ -0,0 +1,15 @@
+#taking input from user
+num1=10
+num2=5
+
+#addition of numbers
+print("Sum of ",num1, " and ",num2," is: ",num1+num2)
+
+#subtraction of numbers
+print("Subtraction of ",num1, " and ",num2," is: ",num1-num2)
+
+#Division of numbers
+print("Division of ",num1, " and ",num2," is: ",num1/num2)
+
+#multiplication of numbers
+print("Multiplication of ",num1, " and ",num2," is: ",num1*num2)
