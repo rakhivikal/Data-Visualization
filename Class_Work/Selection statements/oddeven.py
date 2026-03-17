2fdd43355fa5a5f3743295c8879d99b152163104
Rakhi <rakhivikal008@gmail.com>
2026-03-17 10:26:51 +0530

    Program to check odd or even number

---
+++ Class_Work/Selection statements/oddeven.py
@@ -0,0 +1,10 @@
+# Program to check odd or even number
+
+# Taking input from user
+num = int(input("Enter a number: "))
+
+# Checking condition
+if num % 2 == 0:
+    print("The number is Even")
+else:
+    print("The number is Odd")
