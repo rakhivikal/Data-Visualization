688235fdb82993899a035858a78036ab4c527810
Rakhi <rakhivikal008@gmail.com>
2026-04-09 10:59:40 +0530

    to sort list without sort list

---
+++ Class_Work/Python_coding_challenge-2/positive/negative/sortwithoutsort.py
@@ -0,0 +1,9 @@
+# Taking input from user
+lst = list(map(int, input("Enter numbers: ").split()))
+
+for i in range(len(lst)):
+    for j in range(i+1, len(lst)):
+        if lst[i] > lst[j]:
+            lst[i], lst[j] = lst[j], lst[i]
+
+print(lst)
