fc49b8b9b2f2153a38fae21e761825edc0a47bd4
Rakhi <rakhivikal008@gmail.com>
2026-04-09 10:54:37 +0530

    comman element

---
+++ Class_Work/Python_coding_challenge-2/positive/negative/commanelements.py
@@ -0,0 +1,10 @@
+# Taking input from user
+a = list(map(int, input("Enter list1: ").split()))
+b = list(map(int, input("Enter list2: ").split()))
+
+common = []
+for i in a:
+    if i in b and i not in common:
+        common.append(i)
+
+print(common)
