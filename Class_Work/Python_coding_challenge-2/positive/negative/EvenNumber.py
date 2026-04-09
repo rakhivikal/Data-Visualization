37738fa70cf5ff201ec6ebac14d05bf671d16df8
Rakhi <rakhivikal008@gmail.com>
2026-04-09 10:21:40 +0530

    Program to print even numbers up to n

---
+++ Class_Work/Python_coding_challenge-2/positive/negative/EvenNumber.py
@@ -0,0 +1,11 @@
+
+# Taking input
+n = int(input("Enter number: "))
+
+# Start from 2 (first even number)
+i = 2
+
+# Loop until n
+while i <= n:
+    print(i, end=" ")
+    i += 2   # increment by 2
