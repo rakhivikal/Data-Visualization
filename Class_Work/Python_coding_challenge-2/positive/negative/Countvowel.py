24b18085751cbfc58b94d1260394a136c01a60e3
Rakhi <rakhivikal008@gmail.com>
2026-04-09 10:38:57 +0530

    Program to count number of vowels in a string

---
+++ Class_Work/Python_coding_challenge-2/positive/negative/Countvowel.py
@@ -0,0 +1,17 @@
+# P
+# gitrogram to count number of vowels in a string
+
+# Taking input from user
+text = input("Enter a string: ")
+
+# Initialize vowel counter
+count = 0
+
+# Loop through each character
+for ch in text:
+    # Check if character is vowel
+    if ch.lower() in ['a', 'e', 'i', 'o', 'u']:
+        count += 1   # increase count
+
+# Display result
+print("Number of vowels =", count)
